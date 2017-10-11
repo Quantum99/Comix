e53264c8a30d047e411b4b35c798127f27e63c65
Quantum99 <miketech66@outlook.com>
2017-10-11 11:19:15 -0500

    Update to load the page

--- scapr.py
+++ scapr.py
@@ -1,33 +1,22 @@
-#!/usr/bin/env python
- 
-import gtk, webkit, urllib2
- 
-x=100
- 
-def update_buttons(view, frame, resource, request, response):
-   global x
-   url=request.get_uri()
-   if "jpg?" in url:
-     print url
-     u = urllib2.urlopen(url)
-     file = open( str(x) + '.jpg','w')
-     file.write(u.read())
-     file.close()
-     x+=1
- 
-win = gtk.Window()
-win.connect('destroy', lambda w: gtk.main_quit())
-win.show()
- 
-box1 = gtk.HBox()
-win.add(box1)
- 
-web = webkit.WebView()
-web.connect("resource-request-starting", update_buttons)
-box1.pack_start(web)
- 
-web.open("http://www.mangahere.co/manga/itsuwaribito_utsuho/c001/")
- 
-box1.show_all()
- 
-gtk.main()
+import argparse, time, os,getpass
+import urllib.parse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui as ui
+import datetime
+
+browser = webdriver.PhantomJS()
+browser.set_window_size(1024, 768)
+browser.implicitly_wait(60)
+
+load = ui.WebDriverWait(browser, 10)
+
+def main():
+    browser.get("http://kissmanga.com/Manga/Naruto")
+    load.until(lambda browser: browser.find_element_by_class_name('listing'))
+    browser.save_screenshot('screen1.png')
+
+    browser.quit()
+
+if __name__ == '__main__':
+    main()
